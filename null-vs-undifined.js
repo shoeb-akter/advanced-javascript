4ee123fd69b0298eaf3b02608e2ffc3a5aeaf503
Shoeb Akter <123shoebakter@gmail.com>
2021-02-02 05:36:25 +0600

    addin null vs undifined

---
+++ null-vs-undifined.js
@@ -0,0 +1,19 @@
+// // let pakhi;
+
+function add(num1, num2){
+    console.log(num1 + num2);
+    return  //kisu nai;
+}
+const result = add(5, 2);
+console.log(result);
+
+const premik = {name:"Smart Dude", job: "actor"};
+console.log(premik.gf);
+
+let fun = undifined;
+console.log(fun);
+
+let ages = [12, 14, 13];
+console.log(ages[11]);
+
+//ar null set korte hoy jate developer re bujhe oi jaigai kichu nai
---
+++ true-false.js
@@ -0,0 +1,14 @@
+const name = "";
+
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+// so the falsy comnditions are...
+// 0
+// ""
+// null
+// NaN
+
